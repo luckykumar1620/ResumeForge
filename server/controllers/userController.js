6a8c5b7a9b7c132ff80474c4b2264e4664792109
Lucky Kumar <lucky9110132241@gmail.com>
2026-01-04 12:14:32 +0530

    fix usercontroller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -68,7 +68,7 @@ export const loginUser = async (req, res) => {
         //return success message
         const token = generateToken(user._id)
         user.password = undefined;
-        return res.status(201).json({ message: 'Login successfully', token, user })
+        return res.status(200).json({ message: 'Login successfully', token, user })
 
     } catch (error) {
         return res.status(400).json({ message: error.message })
